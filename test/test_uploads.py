9c8e1e18664cb5b7686c3b59ba4ef9351ea8c2dd
William Dunkerley <william@clio.local>
2019-09-04 20:49:45 -0400

    Tests do not work, but gitignore setup

--- test/test_uploads.py
+++ test/test_uploads.py
@@ -10,34 +10,6 @@ from libs import uploads
 
 class TestUploads:
 
-	# def test_upload(self, client):
-	# 	body = {"filename":"Hellothere.py"}
-	# 	mimetype = 'application/json'
-	# 	headers = {
-	# 		'Content-Type': mimetype,
-	# 		'Accept': mimetype
-	# 	}
-	# 	assert(url_for("upload") == "/upload")
-	# 	res = client.post(url_for('upload'), headers=headers, data=json.dumps(body))
-	# 	assert(res.content_type == mimetype)
-	# 	assert(res.json == body)
-	# 	assert(res.status_code==200)
-
-	# def test_helperfunction(self):
-	# 	dic = {"filename":"Hellothere.py"}
-	# 	assert(uploads.helperfunction(dic)==dic)
-
-	# def test_upload_with_file(self, client):
-	# 	assert(url_for("uploadfile") == "/uploadfile")
-	# 	res = client.post(url_for('uploadfile'),
-	# 		data = {
-	# 			'file': (BytesIO(b'my file contents'), 'hello world.txt'),
-	# 			'othervalue': "hellothere"
-	# 		},
-	# 		content_type="multipart/form-data"
-	# 	)
-	# 	assert({"contents": "my file contents"} == res.json)
-
 	def test_upload(self, client):
 		data = {
 			"filename":"newfile.txt",
