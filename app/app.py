445218125e7f52bebcbe243f77a0f0205a6fe85b
William Dunkerley <hahaveryfun@gmail.com>
2020-11-19 20:53:09 -0500

    chore(app): Cleanup app.py

--- app/app.py
+++ app/app.py
@@ -23,7 +23,6 @@ class PatchedApi(Api):
 
 app = Flask(__name__, instance_relative_config=True)
 CORS(app)
-# app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1)
 
 blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
 api = PatchedApi(blueprint, doc='/doc/', title="Dripbox", authorizations=authorizations, security='jwt')
