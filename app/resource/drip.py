63a2aa83fb8f0a32d902873618278261a8600ca3
William Dunkerley <hahaveryfun@gmail.com>
2020-11-19 20:16:06 -0500

    feat(drip): Correct swagger & jwt documentation for drip

--- app/resource/drip.py
+++ app/resource/drip.py
@@ -2,17 +2,12 @@ from flask_restx import Resource, Namespace
 from flask import send_from_directory
 from app.service import Drip as DripService
 from app.app import api
-import os
-from app.utils.constants import FILE_STORAGE_PATH
+from flask_jwt_extended import jwt_required
 
 api = Namespace('drip', description='Drip operations')
 
 @api.route('/<string:file_uuid>')
 class Drip(Resource):
+	@jwt_required
 	def get(self, file_uuid):
-		drip = DripService().getDrip(file_uuid)
-		return send_from_directory(
-			FILE_STORAGE_PATH,
-			drip.source_identifier,
-			as_attachment=True,
-			attachment_filename=".".join([drip.file_name, drip.file_extension]))
+		return DripService().getDrip(file_uuid)
